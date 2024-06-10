3e0ee0951ab715543a144c2a10133812aac18825
michaelcasey <caseymichaelt1893@gmail.com>
2024-06-10 08:38:45 -0400

    Next search bar added

--- app/src/main/java/com/example/characterapp/app/ui/compose/screens/wire/WireListScreen.kt
+++ app/src/main/java/com/example/characterapp/app/ui/compose/screens/wire/WireListScreen.kt
@@ -9,12 +9,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -27,6 +36,7 @@ import com.example.characterapp.app.ui.viewmodel.WireListViewModel
 import com.example.characterapp.common.state.CommonScreen
 import kotlinx.coroutines.flow.collectLatest
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WireListScreen(
     viewModel: WireListViewModel,
@@ -36,9 +46,37 @@ fun WireListScreen(
         viewModel.submitAction(WireListAction.Load)
     }
 
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+
     viewModel.uiStateFlow.collectAsState().value.let { state ->
         CommonScreen(state = state) {
             Column {
+                SearchBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    query = text,
+                    onQueryChange = {
+                        text = it
+                    },
+                    onSearch =  {
+                        active = false
+                    },
+                    active = active,
+                    onActiveChange = {
+                        active = it
+                    },
+                    placeholder = {
+                        Text(text = "Search")
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search Icon"
+                        )
+                    }
+                ) {
+
+                }
                 WireList(it) { item ->
                     viewModel.submitAction(
                         WireListAction.WireItemClick(
